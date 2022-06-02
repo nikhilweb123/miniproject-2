426f5335625b2d18c5d06c1e5056075fe3f45d6b
nikhil kumar <nikhil.kumar_cs19@gla.ac.in>
2022-06-02 12:00:36 +0530

    upload project on github

---
+++ admin/index.php
@@ -0,0 +1,67 @@
+<html>
+    <head>
+        <title> the indian spices - home page </title >
+        <link rel = "stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!--- menu section starts --->
+        <div class ="menu text-center">
+            <div class="wrapper">
+                
+                <ul>
+                    <li> <a href="#">home</a></li>
+                    <li> <a href="#">admin manger</a></li>
+                    <li> <a href="#">category</a></li>
+                    <li> <a href="#">food</a></li>
+                    <li> <a href="#">order</a></li>
+                </ul>
+             
+            </div>
+        </div>
+        <!--- menu section ends--->
+    <!--- main content section starts ---->
+    <div class="main-content">
+    <div class="wrapper">
+             <h1>DASHBORAD</h1>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                categories
+            </div>
+
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                categories
+            </div>
+
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                categories
+            </div>
+
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                categories
+            </div>
+
+            <div class="clearfix"></div>
+            
+    </div>
+    </div>
+    
+    <!--footer section starts--->
+    <div class ="footer">
+    <div class="wrapper">
+            <p class="text-center"> 2022 all rights reserved , developed by <a href="#"> nikhil kumar</a> <p>
+        </div>
+    </div>
+
+    <!---footer section ends --->
+    </body>
+</html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title> the indian spices - home page </title >
+        <link rel = "stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!--- menu section starts --->
+        <div class ="menu text-center">
+            <div class="wrapper">
+                <h1>DASHBORAD</h1>
+                <ul>
+                    <li> <a href="#">home</a></li>
+                    <li> <a href="#">admin manger</a></li>
+                    <li> <a href="#">category</a></li>
+                    <li> <a href="#">food</a></li>
+                    <li> <a href="#">order</a></li>
+                </ul>
+             
+            </div>
+        </div>
+        <!--- menu section ends--->
+    <!--- main content section starts ---->
+    <div class="main-content">
+    <div class="wrapper">
+            <h1>manage admin</h1>
+            
+            
+    </div>
+    </div>
+    
+    <!--footer section starts--->
+    <div class ="footer">
+    <div class="wrapper">
+            <p class="text-center"> 2022 all rights reserved , developed by <a href="#"> nikhil kumar</a> <p>
+        </div>
+    </div>
+
+    <!---footer section ends --->
+    </body>
+</html>
